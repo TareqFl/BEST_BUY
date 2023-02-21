00502dfd54bd1c219aadf4d792edf40573ce3037
tareq fleyfel <tareq.fleyfel@gmail.com>
2023-02-21 03:45:30 +0300

    adjust

--- src/pages/api/product.js
+++ src/pages/api/product.js
@@ -4,14 +4,22 @@ import Products from "@/models/ProductModel";
 export default async function handleProducts(req, res) {
   await mongoConnect();
   if (req.method === "POST") {
-    const { product } = req.body;
+    try {
+      const { product } = req.body;
 
-    const resp = await Products.insertMany(product);
-    if (resp) {
-      return res.status(200).json(resp);
+      const resp = await Products.insertMany(product);
+      if (resp) {
+        return res.status(200).json(resp);
+      }
+    } catch (error) {
+      res.send(200).json({ message: error.message });
     }
   } else {
-    const resp = await Products.find();
-    return res.status(200).json(resp);
+    try {
+      const resp = await Products.find();
+      return res.status(200).json(resp);
+    } catch (error) {
+      res.send(200).json({ message: error.message });
+    }
   }
 }
